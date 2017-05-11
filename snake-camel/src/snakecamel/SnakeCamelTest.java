5596f9e70f520ffc6b22f8afb9cd880e01ee691c
g15935of <g15935of@m20217.tsuda.ac.jp>
2017-05-11 17:22:21 +0900

    テストについて

--- snake-camel/src/snakecamel/SnakeCamelTest.java
+++ snake-camel/src/snakecamel/SnakeCamelTest.java
@@ -9,20 +9,24 @@ public class SnakeCamelTest {
 
 	@Test
 	public void SnakeToCamelのテストに成功するか(){
-		SnakeCamelUtil sc = new SnakeCamelUtil();
-		String actual = sc.snakeToCamelcase("I_have_an_apple");
+		String actual = SnakeCamelUtil.snakeToCamelcase("i_have_an_apple");
 		String pico = "IHaveAnApple";
+		String actual2 = SnakeCamelUtil.snakeToCamelcase("snake_to_camel");
+		String snake = "SnakeToCamel";
+		assertThat(actual, is(pico));
+		assertThat(actual2, is(snake));
 		
-		assertThat(actual, is(pico));		
 	}
 	
 	@Test
 	public void CamelToSnakeのテストに成功するか(){
-		SnakeCamelUtil sc = new SnakeCamelUtil();
-		String actual = sc.camelToSnakecase("IHaveAnApple");
+		String actual = SnakeCamelUtil.camelToSnakecase("IHaveAnApple");
 		String pico = "i_have_an_apple";
+		String actual2 = SnakeCamelUtil.camelToSnakecase("CamelToSnake");
+		String camel = "camel_to_snake";
 		
 		assertThat(actual, is(pico));		
+		assertThat(actual2, is(camel));
 		
 	}
 }
