14250859816d28a18668024a4f764d0cdeac8d96
g15935of <g15935of@m20217.tsuda.ac.jp>
2017-05-11 17:31:19 +0900

    こんどこそ

--- snake-camel/src/snakecamel/SnakeCamelTest.java
+++ snake-camel/src/snakecamel/SnakeCamelTest.java
@@ -10,10 +10,13 @@ public class SnakeCamelTest {
 	@Test
 	public void SnakeToCamelのテストに成功するか(){
 		SnakeCamelUtil sc = new SnakeCamelUtil();
+		
 		String actual = sc.snakeToCamelcase("i_have_an_apple");
 		String pico = "IHaveAnApple";
+		
 		String actual2 = sc.snakeToCamelcase("snake_to_camel");
 		String snake = "SnakeToCamel";
+		
 		assertThat(actual, is(pico));
 		assertThat(actual2, is(snake));
 		
@@ -21,12 +24,16 @@ public class SnakeCamelTest {
 	
 	@Test
 	public void CamelToSnakeのテストに成功するか(){
-		String actual = SnakeCamelUtil.camelToSnakecase("IHaveAnApple");
+		SnakeCamelUtil sc = new SnakeCamelUtil();
+		
+		String actual = sc.camelToSnakecase("IHaveAnApple");
 		String pico = "i_have_an_apple";
-		String actual2 = SnakeCamelUtil.camelToSnakecase("CamelToSnake");
+		
+		String actual2 = sc.camelToSnakecase("CamelToSnake");
 		String camel = "camel_to_snake";
 		
-		assertThat(actual, is(pico));		
+		assertThat(actual, is(pico));
+		
 		assertThat(actual2, is(camel));
 		
 	}
